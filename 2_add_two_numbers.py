b2f3d56883384a7f847096a95b17e22bafb81ad8
sul-otaibi <93071482+sul-otaibi@users.noreply.github.com>
2022-03-24 18:57:00 +0300

    Create 2_add_two_numbers.py

---
+++ 2_add_two_numbers.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/add-two-numbers/
+
+def addTwoNumbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    if 0 == l1.val and l1.next is None:
+        return l2
+    if 0 == l2.val and l2.next is None:
+        return l1
+    
+    power = 1
+    num1 = l1.val
+    while l1.next is not None:
+        l1 = l1.next
+        num1 += l1.val * 10 ** power
+        power += 1
+    
+    power = 1
+    num2 = l2.val
+    while l2.next is not None:
+        l2 = l2.next
+        num2 += l2.val * 10 ** power
+        power += 1
+
+    the_sum = num1 + num2
+    result = ListNode(the_sum % 10)
+
+    temp = result
+    while the_sum // 10 != 0:
+        the_sum //= 10
+        temp.next = ListNode(the_sum % 10)
+        temp = temp.next
+    return result
